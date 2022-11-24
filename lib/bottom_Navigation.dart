691996352271e574fa9e933cb8c3011ce55eaf4b
ReYAp047 <meftah.dali@gmail.com>
2022-11-24 13:24:26 +0100

    Adding Bottom tab navigation

---
+++ lib/bottom_Navigation.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:parkinny/screen/order_traking_page.dart';
+
+
+class bottom_Navigation extends StatelessWidget {
+  const bottom_Navigation({super.key});
+
+  //static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+  Center(
+  child: OrderTrackingPage(),
+  ),
+    Text(
+      'Index 1: Wallet',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Historique',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     /* appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),*/
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_outlined),
+            label: 'Map',
+           // backgroundColor: Colors.indigo,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet_outlined),
+            label: 'Wallet',
+           // backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history_outlined),
+            label: 'Historique',
+           // backgroundColor: Colors.purple,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.indigo,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
