0d999f066f22981edcf82504e490542130167206
ReYAp047 <meftah.dali@gmail.com>
2022-11-24 14:40:59 +0100

    Adding area polygon zone

--- lib/screen/order_traking_page.dart
+++ lib/screen/order_traking_page.dart
@@ -15,8 +15,8 @@ class OrderTrackingPage extends StatefulWidget {
 class OrderTrackingPageState extends State<OrderTrackingPage> {
   final Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng sourceLocation = LatLng(37.4221, -122.0841);
-  static const LatLng destination = LatLng(37.4116, -122.0713);
+  static const LatLng sourceLocation = LatLng(36.8978, 10.1901);
+  static const LatLng destination = LatLng(36.8580, 10.1844);
 
   List<LatLng> polylinecordinates = [];
   LocationData? currentLocation;
@@ -24,6 +24,14 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
   BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
   BitmapDescriptor destinationIcon = BitmapDescriptor.defaultMarker;
   BitmapDescriptor currentLocationIcon = BitmapDescriptor.defaultMarker;
+
+  List<LatLng> polygonPoints = const[
+    LatLng(36.857020, 10.181680),
+    LatLng(36.851583, 10.183963),
+    LatLng(36.857867, 10.190897),
+    LatLng(36.861780, 10.187057),
+    LatLng(36.860009, 10.181437),
+  ];
 
   void getCurrentLocation () async {
     Location location = Location();
@@ -138,6 +146,27 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
               markerId: const MarkerId("destination"),
               position: destination
           ),
+        },
+        /*
+        circles:
+          {
+            Circle(
+              circleId: CircleId("1"),
+              center: LatLng(
+          currentLocation!.latitude!, currentLocation!.longitude!),
+              radius: 430,
+              strokeWidth: 2,
+              fillColor: Color(0xFF006491).withOpacity(0.2),
+            ),
+          },*/
+        polygons: {
+          Polygon(
+            polygonId: const PolygonId("1"),
+            points: polygonPoints,
+            strokeWidth: 2,
+            strokeColor: const Color(0xFF006491),
+            fillColor: const Color(0xFF006491).withOpacity(0.2),
+          )
         },
         onMapCreated: (mapController){
           _controller.complete(mapController);
